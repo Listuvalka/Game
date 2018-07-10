b0f29e1d3ef7614b64b1afbe2caf416ab911d43d
Vera <verayereschenko@gmail.com>
2018-07-10 17:06:03 +0300

    SpellBook delete

--- Spell/WizardSpellBook.cpp
+++
@@ -1,7 +0,0 @@
-#include "WizardSpellBook.h"
-
-WizardSpellBook::WizardSpellBook(): SpellBook(new Fireball(), FIREBALL) {
-    addSpell(new Lightning(), LIGHTINING);
-    addSpell(new Heal(), HEAL);
-}
-WizardSpellBook::~WizardSpellBook() {}
